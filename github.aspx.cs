5e26f927f27b07b2251606f6473d571d004fcd0b
fiach <fiach.reid@gmail.com>
2018-08-15 11:00:11 +0100

    Added repo clone code

--- github.aspx.cs
+++ github.aspx.cs
@@ -1,17 +1,12 @@
 ﻿using System;
+using LibGit2Sharp;
+
+// Install-Package LibGit2Sharp -Version 0.24
 
 public partial class github : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        string headers = String.Empty;
-        foreach (string key in Request.Headers.AllKeys)
-            headers += key + "=" + Request.Headers[key] + Environment.NewLine;
-        headers += Environment.NewLine + Environment.NewLine + Request.Url.Query;
-
-        foreach (string key in Request.Form.AllKeys)
-            headers += key + "=" + Request.Form[key] + Environment.NewLine;
-
-        Email.Send("fiach.reid@gmail.com", "GitHub webhook", headers);
+        Repository.Clone("https://github.com/infiniteloopltd/AutoBuild.git", @"E:\wwwroot\autobuild.webtropy.com\", null);  
     }
 }
