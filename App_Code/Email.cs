b8a27d26633b2f68b32a867db0eb3486be93dbb3
fiach <fiach.reid@gmail.com>
2018-08-15 10:43:05 +0100

    Added initial files

---
+++ App_Code/Email.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Email
+/// </summary>
+public class Email
+{
+	public Email()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
