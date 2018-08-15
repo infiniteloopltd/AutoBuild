b75dcf060de3710bfd7c054e4869397cc75380ba
fiach <fiach.reid@gmail.com>
2018-08-15 10:45:17 +0100

    Added email

--- github.aspx.cs
+++ github.aspx.cs
@@ -1,14 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class github : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        string headers = String.Empty;
+        foreach (string key in Request.Headers.AllKeys)
+            headers += key + "=" + Request.Headers[key] + Environment.NewLine;
+        headers += Environment.NewLine + Environment.NewLine + Request.Url.Query;
 
+        foreach (string key in Request.Form.AllKeys)
+            headers += key + "=" + Request.Form[key] + Environment.NewLine;
+
+        Email.Send("fiach.reid@gmail.com", "GitHub webhook", headers);
     }
 }
