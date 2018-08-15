4e42985ffe6b52713ccfcea0d7ca3413aae28a5b
Fiach Reid <fiach.reid@gmail.com>
2018-08-15 11:37:08 +0100

    Trying GitSharp

--- github.aspx.cs
+++ github.aspx.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using LibGit2Sharp;
-
-// Install-Package LibGit2Sharp -Version 0.24
+using GitSharp;
+// Install-Package GitSharp
 
 public partial class github : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        Repository.Clone("https://github.com/infiniteloopltd/AutoBuild.git", @"E:\wwwroot\autobuild.webtropy.com\", null);  
+        Git.Clone("https://github.com/infiniteloopltd/AutoBuild.git", @"E:\wwwroot\autobuild.webtropy.com\");  
     }
 }
